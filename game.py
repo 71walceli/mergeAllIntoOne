5f8e7d8bee82d24307fd2e3a866fc0ce0dc00c60
apk <antonini2903@hotmail.com>
2020-03-30 21:57:12 -0500

    20200329-214851
    - Changed all occurencies of fameGrid to self.grid
    - Refactored to be object-oriented:
        - fall()
        - howFullIsGrid()
        - isGridFull()
        - makeMerges( work-in-pregress)

--- game.py
+++ game.py
@@ -39,7 +39,7 @@ class X2BlocksCloneCliImpl:
       return -1
 
   # May no longer be needed as __init__ constructor can build the grid.
-  #def buildGameGrid(self, width: int, height: int):
+  #def buildself._grid(self, width: int, height: int):
   #  """
   #  Constructs game grid of given width and height
   #  """
@@ -50,6 +50,9 @@ class X2BlocksCloneCliImpl:
     Makes the blocks fall into the empty spaces (the zeroes) until all blocks have
     fallen to the very bottom of the grid and stack one over others that are below
     in the same column.
+
+    TODO Separate logic to make each column fall independently, as one column's
+    falling is independent, except for merging.
     """
 
     for column in range(self._width):
@@ -65,7 +68,7 @@ class X2BlocksCloneCliImpl:
 
   def howFullIsGrid(self):
     """
-    return a percentage of how much of gameGrid is full, that is, all of its cells
+    return a percentage of how much of self._grid is full, that is, all of its cells
     have non-zero values.
     """
     factor = 1 /(self._width *self._height)
@@ -78,46 +81,69 @@ class X2BlocksCloneCliImpl:
     
     return round(percentage, ndigits=2)
 
-  def isGridFull(self, gameGrid):
+  def isGridFull(self):
     """
-    Checks if gameGrid is full by checking if the top-most row is full of blocks.
+    Checks if self._grid is full by checking if the top-most row is full of blocks.
     """
-    topmostRow = len(gameGrid) -1
-    count = len(gameGrid[topmostRow])
+    topmostRow = self._height -1  # index fir topmost row
+    count = width
 
-    for block in gameGrid[topmostRow]:
+    for block in self._grid[topmostRow]:
       if block != 0:
-        count -= 1
-    return count == 0
+        count -= 1     # count down for every used space.
+    return count == 0  # returns trie if the top row is full.
 
-  def makeMerges(self, gameGrid, column):
-    row = -1
-    for _row in range(len(gameGrid)):
-      if gameGrid[_row][column] != 0:
-        row += 1
-      else:
-        break
+  def makeMerges(self, column):
+    """
+    Makes all the merges in a column happen. The merging occurs in this way:
 
-    checkBlock  = gameGrid[row][column]
+    1. A block's value is polled and stored.
+    2. The stored value is compared for equality to the left block's value, and
+    merged into the block whose value was stored.
+    3. As in the previous spep, the block on the righy is merged if the stored and
+    the block's value match.
+    4. The block over the one checked is merged downward as explained.
+    
+    The result is a block incremented ince for every succesful merge, at the same
+    positoon as the block whose value is taken, unless it was avove anotheer piece
+    that had equal valuem, in which case, it will be one place below. All merges can
+    hapoen atonce.
+
+    TODO 1 Make a routine out of the logic contained here so that it can be called
+    for a siven position in the grid, such as the exact position a block was thrown.
+
+    TODO 2 Refactor this routine to do all posible merges in the given column
+    """
+
+    row = -1                             # As of TODO 1, this will be deleted, as it
+    for _row in range(self._height):     # would fetch the totmost block only to do
+      if self._grid[_row][column] != 0:  # the merging. Uswlwss if more than one
+        row += 1                         # merges can happen.
+      else:                              #  
+        break                            #  
+
+    checkBlock  = self._grid[row][column]
+    
+    # Indexes to check surrounging blocks
     columnLeft  = column -1
     columnRight = column +1
     rowBelow    = row -1
 
-    if column != 0                       and gameGrid[row][columnLeft] == checkBlock :
-      gameGrid[row][column]      += 1
-      gameGrid[row][columnLeft]   = 0
-      makeMerges(gameGrid, column)
+    if column != 0                       and self._grid[row][columnLeft] == checkBlock :
+      self._grid[row][column]      += 1
+      self._grid[row][columnLeft]   = 0
+      makeMerges(self._grid, column)
     
-    if columnRight != len(gameGrid[row]) and gameGrid[row][columnRight] == checkBlock:
-      gameGrid[row][column]      += 1
-      gameGrid[row][columnRight]  = 0
-      makeMerges(gameGrid, column)
+    if columnRight != len(self._grid[row]) and self._grid[row][columnRight] == checkBlock:
+      self._grid[row][column]      += 1
+      self._grid[row][columnRight]  = 0
+      makeMerges(self._grid, column)
     
-    if row != 0 and gameGrid[rowBelow][column] == checkBlock:
-      actualBlock                = gameGrid[row][column] +1
-      gameGrid[rowBelow][column] = actualBlock
-      gameGrid[row][column]      = 0
-      makeMerges(gameGrid, column)
+    if row != 0 and self._grid[rowBelow][column] == checkBlock:
+      actualBlock                = self._grid[row][column] +1
+      self._grid[rowBelow][column] = actualBlock
+      self._grid[row][column]      = 0
+      makeMerges(self._grid, column)
 
   def nextBlock(self, ):
     """
@@ -132,24 +158,24 @@ class X2BlocksCloneCliImpl:
       return randint(1, blockHighest)
     return blockTemp
 
-  def play(self, gameGrid):
+  def play(self, self._grid):
     """
     Manages all the gameplay logic.
     """
-    while not isGridFull(gameGrid):
+    while not isGridFull(self._grid):
       # TODO: Tidy up login for every turn
       # TODO: Handle all excpetions
-      takeTurn(gameGrid)
-      printGrid(gameGrid)
+      takeTurn(self._grid)
+      printGrid(self._grid)
 
-  def printGrid(self, gameGrid):
-    for row in gameGrid:
+  def printGrid(self, self._grid):
+    for row in self._grid:
       for cell in row:
         print(cell, end=" ")
       print()
     print()
 
-  def shoot(self, gameGrid, block, column):
+  def shoot(self, self._grid, block, column):
     """
     Puts blocks in the bottom most row
     """
@@ -157,18 +183,18 @@ class X2BlocksCloneCliImpl:
     # loop.
     # _row os just an index to move though the column. The following loop stops when
     # it finds the bottom most empty spot to put block in.
-    for _row in range(len(gameGrid)):
+    for _row in range(len(self._grid)):
       row += 1
-      if gameGrid[_row][column] == 0:
+      if self._grid[_row][column] == 0:
         break
     
     # Put the block in the bottom most blank
-    if gameGrid[row][column] == 0:
-      #gameGrid[row][column] == 0
-      gameGrid[row][column] = block
+    if self._grid[row][column] == 0:
+      #self._grid[row][column] == 0
+      self._grid[row][column] = block
       blockTemp = 0
-    elif gameGrid[row][column] == block:
-      gameGrid[row][column] += 1
+    elif self._grid[row][column] == block:
+      self._grid[row][column] += 1
       blockTemp = 0
     else:
       blockTemp = block
@@ -176,20 +202,20 @@ class X2BlocksCloneCliImpl:
       # TODO: Throw custom exception for the game
 
 
-  def takeTurn(self, gameGrid):
+  def takeTurn(self, self._grid):
     """
     Play a turn every time. Lets exceptions spread, which the main game loop should
     handle and act accordingly.
     """
     block = nextBlock()
     print(f"Block: {block}")
-    column = aimAt(gameGrid)    # what column to shoot at?
+    column = aimAt(self._grid)    # what column to shoot at?
     if column in range(width):  # range() domain is [0, width -1]
-      shoot(gameGrid, block, column)  # FIXME when block isn't shot, return the 
+      shoot(self._grid, block, column)  # FIXME when block isn't shot, return the 
       # previous one, to prevent the player from thowing away blocks.
       # TODO Improve self-arranging logic to fall() and merge() when it must
-      makeMerges(gameGrid, column)    # TODO more testing!
-      fall(gameGrid)
+      makeMerges(self._grid, column)    # TODO more testing!
+      fall(self._grid)
     else:
       print("Invalid input")
       blockTemp = block
